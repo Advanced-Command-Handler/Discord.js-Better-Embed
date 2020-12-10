4717605be9d40f0cd3f537c8f7e43e9a475c969c
Ayfri <pierre.ayfri@gmail.com>
2020-12-10 08:48:36 +0100

    Added BetterEmbed class.

---
+++ src/BetterEmbed.ts
@@ -0,0 +1,48 @@
+import {MessageEmbed} from 'discord.js';
+
+export default class BetterEmbed extends MessageEmbed {
+	public static limits = {
+		author: {
+			name: 256,
+		},
+		title: 256,
+		description: 2048,
+		footer: {
+			text: 2048,
+		},
+		fields: {
+			size: 25,
+			name: 256,
+			value: 1024,
+		},
+	}
+	
+	public static templates = {
+		basic: {
+			footer: {
+				text: '${client.user.username}',
+				iconURL: '${client.user.displayAvatarURL()}',
+			},
+			timestamp: new Date(),
+		},
+		color: {
+			color: '#4b5afd',
+		},
+		get complete() {
+			return {
+				...this.basic,
+				...this.color,
+				title: '${title}',
+				description: '${description}',
+			};
+		},
+		get image() {
+			return {
+				...this.complete,
+				image: {
+					url: '${image}',
+				},
+			};
+		},
+	};
+}
