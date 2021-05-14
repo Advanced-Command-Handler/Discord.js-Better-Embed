364017adac1869c7ee5b53fbaa1fa672193b6678
Ayfri <pierre.ayfri@gmail.com>
2021-05-14 23:57:52 +0200

    style: Prettify code.

--- src/BetterEmbed.ts
+++ src/BetterEmbed.ts
@@ -4,7 +4,7 @@ import path from 'path';
 type AnyObject = {[k: string]: any};
 
 type Template = MessageEmbedOptions;
-type Templates = { [k in string | 'basic' | 'color' | 'complete' | 'image']: Template }
+type Templates = {[k in string | 'basic' | 'color' | 'complete' | 'image']: Template};
 
 type CheckSizeKey = keyof Template | string;
 type CheckSizeContent = Template[keyof Template];
@@ -57,19 +57,19 @@ export const limits = {
 export class BetterEmbed extends MessageEmbed {
 	public static LENGTH_LIMITS = limits;
 	public static TEMPLATES = templates;
-	
+
 	public constructor(data?: MessageEmbed | Template) {
 		super(data);
 		this.checkSize();
 	}
-	
+
 	public static fromTemplate(template: keyof Templates | Template, values: AnyObject): BetterEmbed {
 		if (typeof template === 'string')
 			if (templates[template]) template = templates[template];
 			else throw new Error(`Template '${template}' not found.`);
-		
+
 		template = JSON.parse(JSON.stringify(template));
-		
+
 		function setValues(object: AnyObject, values: AnyObject): Template {
 			for (const [name, value] of Object.entries(object)) {
 				if (!object.hasOwnProperty(name)) continue;
@@ -78,34 +78,33 @@ export class BetterEmbed extends MessageEmbed {
 					object[name] = setValues(value, values);
 					continue;
 				}
-				
+
 				const code = value.replace(/\$\{([^}]+)\}/gu, (_: any, value: string) => {
-						return values.hasOwnProperty(value.split('.')[0]) ? `\${values.${value}}` : value;
-					},
-				);
+					return values.hasOwnProperty(value.split('.')[0]) ? `\${values.${value}}` : value;
+				});
 				object[name] = eval(`\`${code}\``);
 			}
-			
+
 			return object;
 		}
-		
+
 		return new BetterEmbed(setValues(template as AnyObject, values));
 	}
-	
-	public checkSize(field: 'fields'): {index: number, limit: number} & ({name: boolean} | {value: boolean}) | boolean
+
+	public checkSize(field: 'fields'): ({index: number; limit: number} & ({name: boolean} | {value: boolean})) | boolean;
 	public checkSize(field: keyof Template): boolean;
-	public checkSize(): { [k in CheckSizeKey]: {content: CheckSizeContent, limit: number} }
+	public checkSize(): {[k in CheckSizeKey]: {content: CheckSizeContent; limit: number}};
 	public checkSize(field?: keyof Template) {
 		if (!field) {
-			const fields: { [k in CheckSizeKey]: {content: CheckSizeContent, limit: number} } = {};
-			
+			const fields: {[k in CheckSizeKey]: {content: CheckSizeContent; limit: number}} = {};
+
 			function addField(name: CheckSizeKey, content: CheckSizeContent, limit: number) {
 				fields[name] = {
 					content,
 					limit,
 				};
 			}
-			
+
 			if (this.title && this.title.length > limits.title) addField('title', this.title, limits.title);
 			if (this.author?.name && this.author.name.length > limits.author.name) addField('author', this.author.name, limits.author.name);
 			if (this.description && this.description.length > limits.description) addField('description', this.description, limits.description);
@@ -114,10 +113,10 @@ export class BetterEmbed extends MessageEmbed {
 				if (field.name?.length > limits.fields.name) addField(`field[${index}]`, field.name, limits.fields.name);
 				if (field.value?.length > limits.fields.value) addField(`field[${index}]`, field.value, limits.fields.value);
 			});
-			
+
 			return fields;
 		}
-		
+
 		switch (field) {
 			case 'fields':
 				if (this.fields?.length) {
@@ -153,19 +152,19 @@ export class BetterEmbed extends MessageEmbed {
 				return true;
 		}
 	}
-	
+
 	public setImageFromFile(link: string) {
 		const attachment = new MessageAttachment(link, path.basename(link));
 		this.attachFiles([attachment]);
 		this.setImage(`attachment://${attachment.name}`);
 	}
-	
+
 	public setThumbnailFromFile(link: string) {
 		const attachment = new MessageAttachment(link, path.basename(link));
 		this.attachFiles([attachment]);
 		this.setThumbnail(`attachment://${attachment.name}`);
 	}
-	
+
 	public throwIfTooLong(field: keyof Template): void;
 	public throwIfTooLong(field?: keyof Template) {
 		if (field) {
@@ -177,7 +176,7 @@ export class BetterEmbed extends MessageEmbed {
 				case 'description':
 					if (field === 'author' ? !this.author?.name?.length : !this[field]?.length) return;
 					const name = field === 'author' ? 'author.name' : field;
-					
+
 					const limit = field === 'author' ? limits.author.name : limits[field];
 					const length = field === 'author' ? this.author!.name!.length : this[field]!.length;
 					throw new RangeError(`'embed.${name}' is too long: ${length} (max: ${limit}).`);
@@ -191,7 +190,7 @@ export class BetterEmbed extends MessageEmbed {
 					}
 			}
 		}
-		
+
 		if (this.title && this.title.length > limits.title) throw new RangeError(`'embed.title' is too long: ${this.title.length} (max: ${limits.title}).`);
 		if (this.author?.name && this.author.name.length > limits.author.name) throw new RangeError(`'embed.author.name' is too long: ${this.author.name.length} (max: ${limits.author.name}).`);
 		if (this.description && this.description.length > limits.description) throw new RangeError(`'embed.description' is too long: ${this.description.length} (max: ${limits.description}).`);
@@ -202,12 +201,12 @@ export class BetterEmbed extends MessageEmbed {
 				throw new RangeError(`'embed.fields[${this.fields.indexOf(field)}].value' is too long: ${field.value.length} (max: ${limits.fields.value}).`);
 		});
 	}
-	
+
 	public cutIfTooLong() {
 		function cutWithLength(text: string, maxLength: number) {
 			return text.length > maxLength ? `${text.substring(0, maxLength - 3)}...` : text;
 		}
-		
+
 		if (this.author?.name) this.author.name = cutWithLength(this.author.name, limits.author.name);
 		if (this.description) this.description = cutWithLength(this.description, limits.description);
 		if (this.title) this.title = cutWithLength(this.title, limits.title);
